df87267893022e7137935dd34adaa8c9cd7600d6
Star-ica <akangbeabigael11@gmail.com>
2026-09-14 23:59:42 +0100

    update

--- Smartcontract/script/Deploy.s.sol
+++ Smartcontract/script/Deploy.s.sol
@@ -11,7 +11,7 @@ contract Deploy is Script {
         vm.startBroadcast(deployer);
         
         // Replace with your ERC20 token address
-        address tokenAddress = 0x75edC9335175Fc0552D51D48439F229c10420fe3;
+        address tokenAddress = 0xaBabc7Ddc03e501d190C676BF3d92ef0e6e87a3C;
 
         SpinReward spinReward = new SpinReward(tokenAddress);
 
